00899a39ed4ca6cb56548b3bd8b7109d2b3ed6b3
olivier <olivier@localhost.localdomain>
2015-08-20 20:45:48 +0200

    feature/21 - Repository design pattern implemented

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Email;
 use App\Http\Requests\EmailRequest;
+#use App\Repositories\EmailRepository;       // Solution 2 
+use App\Repositories\EmailRepositoryInterface;       // Solution 3
 
 class EmailController extends Controller {
     
     public function getForm() {
         return view('email');
     }
+   
+    // Solution 0 
     
-    public function postForm(EmailRequest $request) {
+    /*public function postForm(EmailRequest $request) {
         
         $email = new Email;
         $email->email = $request->input('email');
@@ -19,6 +23,31 @@ class EmailController extends Controller {
         
         return view('email_success');
         
+    }*/
+    
+    // Solution 1 
+    
+    /*public function postForm(EmailRequest $request, Email $email) {
+            
+        $email->email = $request->input('email');
+        $email->save();
+        
+        return view('email_success');
+        
+    }*/
+    
+    // Solution 2
+    
+    /*public function postForm(EmailRequest $request, EmailRepository $emailRepo) {
+        $emailRepo->save($request->input('email'));        
+        return view('email_success');        
+    }*/
+    
+    // Solution 3
+    
+    public function postForm(EmailRequest $request, EmailRepositoryInterface $emailRepo) {
+        $emailRepo->save($request->input('email'));        
+        return view('email_success');        
     }
     
 }
--- resources/views/email_success.blade.php
+++ resources/views/email_success.blade.php
@@ -9,7 +9,7 @@
         <div class="panel panel-info">
             <div class="panel-heading">Envoi d'une image</div>
             <div class="panel-body">
-                Votre adresse eélectronique a bien été envoyée.
+                Votre adresse électronique a bien été envoyée.
             </div>
         </div>
     </div>
